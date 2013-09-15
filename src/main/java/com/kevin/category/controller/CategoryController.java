c8208d5b6c61ac4034756f6063965d9d8f419bdd
qingwei201314 <87863823@qq.com>
2013-09-15 23:03:41 +0800

    home

--- src/main/java/com/kevin/category/controller/CategoryController.java
+++ src/main/java/com/kevin/category/controller/CategoryController.java
@@ -1,5 +1,8 @@
 package com.kevin.category.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.enterprise.context.RequestScoped;
 import javax.faces.context.FacesContext;
 import javax.inject.Inject;
@@ -15,7 +18,8 @@ import com.kevin.util.Constant;
 public class CategoryController {
 	@Inject
 	private CategoryService categoryService;
-	private Category category = new Category();;
+	private Category category = new Category();
+	private List<Category> categoryList = new ArrayList<Category>();
 
 	public String saveCategory(){
 		HttpSession session = ((HttpServletRequest)FacesContext.getCurrentInstance().getExternalContext().getRequest()).getSession();
@@ -31,5 +35,18 @@ public class CategoryController {
 	public void setCategory(Category category) {
 		this.category = category;
 	}
-	
+
+	/**
+	 * addCategory.xhtml中取得分类列表
+	 */
+	public List<Category> getCategoryList() {
+		HttpSession session = ((HttpServletRequest)FacesContext.getCurrentInstance().getExternalContext().getRequest()).getSession();
+		String phone = (String)session.getAttribute(Constant.phone);
+		categoryList = categoryService.list(phone);
+		return categoryList;
+	}
+
+	public void setCategoryList(List<Category> categoryList) {
+		this.categoryList = categoryList;
+	}
 }
